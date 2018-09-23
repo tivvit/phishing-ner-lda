919c5a4d735b792d451860fa22e6a44f86a873f0
Vit Listik <tivvit@seznam.cz>
2018-09-23 17:12:14 +0200

    200 stonger learners

--- phish.py
+++ phish.py
@@ -59,7 +59,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y,
                                                     random_state=42)
 print("train size: {} test size: {}".format(len(X_train), len(X_test)))
 
-clf = RandomForestClassifier(n_estimators=100, max_depth=2, random_state=0)
+clf = RandomForestClassifier(n_estimators=200, max_depth=5, random_state=0)
 clf.fit(X_train, y_train)
 
 y_pred = clf.predict(X_test)
